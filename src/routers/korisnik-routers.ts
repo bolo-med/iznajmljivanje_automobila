0ce2e5aa2305bbb86fc125a3aba3c7881edfb648
bolo-med <bosko.medojevic@t-com.me>
2020-08-24 15:53:06 +0200

    korisnikove stranice

--- src/routers/korisnik-routers.ts
+++ src/routers/korisnik-routers.ts
@@ -10,12 +10,12 @@ let auth = expressjwt({
     algorithms: ['HS256']
 });
 
-korisnikRouter.route('/korisnici').get(korisnikControllers.getAllKorisnici)
-                                  .post(korisnikControllers.insertKorisnik)
-                                  .put(korisnikControllers.updateKorisnik);
+korisnikRouter.route('/korisnici').get(auth, korisnikControllers.getAllKorisnici)
+                                  .post(auth, korisnikControllers.insertKorisnik)
+                                  .put(auth, korisnikControllers.updateKorisnik);
 
 korisnikRouter.route('/korisnici/:id').get(auth, korisnikControllers.getKorisnikByID)
-                                      .delete(korisnikControllers.deleteKorisnik);
+                                      .delete(auth, korisnikControllers.deleteKorisnik);
 
 korisnikRouter.post('/register', korisnikControllers.register);
 korisnikRouter.post('/login', korisnikControllers.login);
